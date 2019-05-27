c760adbb9d1713b59b4e98e4e8acfcb7005f8579
hak7s <hak7s@hotmail.fr>
2019-05-27 18:30:43 +0200

    lundi

--- model/arme.js
+++ model/arme.js
@@ -1,5 +1,5 @@
 class Arme extends ElementMap {
-    constructor(classCss, degat,position) {
+    constructor(classCss, degat,position=null) {
         super(classCss);
         this.degat= degat;
         this.position=position;
--- model/player.js
+++ model/player.js
@@ -2,6 +2,8 @@ class Player extends ElementMap {
     constructor (classCss, sante) {
         super(classCss);
         this.sante = sante;
-        
+    }
+    ajouteArme(arme){
+        this.arme=arme
     }
 }
